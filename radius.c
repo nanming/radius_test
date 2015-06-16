e103ed9af81aa4e124c712207384ead58025b2ee
hover <hover@netmoon.cn>
2015-06-16 17:45:59 +0800

    add radius_acct_start function

--- radius.c
+++ radius.c
@@ -150,7 +150,7 @@ static int rc_check_reply (AUTH_HDR *auth, int bufferlen, char *secret,
 }
 #endif
 
-int main(int argc, char ** argv)
+static int radius_pap_auth(char *username, char *password)
 {
 	AUTH_HDR *auth, *recv_auth;
 	struct sockaddr salocal; struct sockaddr saremote;
@@ -170,8 +170,8 @@ int main(int argc, char ** argv)
 	unsigned char   passbuf[MAX(AUTH_PASS_LEN, CHAP_VALUE_LENGTH)];
 	unsigned char *pw_buf, *pw_vector;
 
-	char *username = "user1";
-	char *password = "123456";
+	/*char *username = "user1";*/
+	/*char *password = "123456";*/
 	int padded_length;
 	int secretlen;
 	int pc, i;
@@ -259,16 +259,14 @@ int main(int argc, char ** argv)
         }
 
 	total_length += padded_length + 2 + 20;
+	auth->length = htons ((unsigned short) total_length);
 
-	
 	sin = (struct sockaddr_in *) & saremote;
 	memset ((char *) sin, '\0', sizeof (saremote));
 	sin->sin_family = AF_INET;
 	/*sin->sin_addr.s_addr = inet_addr("192.168.0.99");*/
 	sin->sin_addr.s_addr = inet_addr("115.29.203.202");
 	sin->sin_port = htons(1812);
-
-	auth->length = htons ((unsigned short) total_length);
 
 	for (;;)
 	{
@@ -312,6 +310,171 @@ int main(int argc, char ** argv)
 	}
 	/*result = rc_check_reply (recv_auth, 8192, secret, vector, 1);*/
 
+}
+
+static int radius_acct_start(char *username)
+{
+	AUTH_HDR *auth, *recv_auth;
+	struct sockaddr salocal; struct sockaddr saremote;
+	struct sockaddr_in *sin;
+	struct timeval  authtime;
+	fd_set          readfds;
+	char            send_buffer[BUFFER_LEN];
+	char            recv_buffer[BUFFER_LEN];
+	unsigned char *buf;
+	unsigned char vector[AUTH_VECTOR_LEN];
+	unsigned char md5buf[256];
+	int sockfd;
+	unsigned int length;
+	unsigned char secret[49];
+	unsigned int total_length = 0;
+	unsigned int salen;
+	unsigned char   passbuf[MAX(AUTH_PASS_LEN, CHAP_VALUE_LENGTH)];
+	unsigned char *pw_buf, *pw_vector;
+
+	int padded_length;
+	int secretlen;
+	int pc, i;
+
+	char *session_id = username;
+	int acct_status = 1;
+	unsigned int lvalue;
+	/*unsigned long framed_ip_addr;*/
+	unsigned int framed_ip_addr;
+
+	auth = malloc(sizeof(AUTH_HDR));
+	if (auth == (AUTH_HDR *)NULL){
+		printf("alloc auth failed\n");
+		return -1;
+	}
+
+	auth = (AUTH_HDR *)send_buffer;
+	buf = auth->data; 
+	auth->code = 4;
+	auth->id = 1;
+	
+
+	sockfd = socket (AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		printf("sockfd failed\n");
+		return; 
+	}
+
+	length = sizeof (salocal);
+	sin = (struct sockaddr_in *) & salocal;
+	memset ((char *) sin, '\0', (size_t) length);
+	sin->sin_family = AF_INET;
+	sin->sin_addr.s_addr = htonl(INADDR_ANY);
+	sin->sin_port = htons ((unsigned short) 0);
+	if (bind (sockfd, (struct sockaddr *) sin, length) < 0 ||
+		   getsockname (sockfd, (struct sockaddr *) sin, &length) < 0)
+	{
+		close (sockfd);
+		memset (secret, '\0', sizeof (secret));
+		/*error("rc_send_server: bind: %s: %m", server_name);*/
+		/*return (ERROR_RC);*/
+		printf("bind error\n");
+		return -1;
+	}
+
+	strcpy(secret, MGMT_POLL_SECRET);
+
+	 /*username*/ 
+	*buf++ = 1; /*username*/ 
+	*buf++ = 2 + strlen(username);
+	memcpy(buf, username, strlen(username));
+	buf += strlen(username) ;
+	total_length += 2 + strlen(username);
+
+    /*acct-session-id*/ 
+	*buf++ = 44;
+	*buf++ = 2 + strlen(session_id);
+	memcpy(buf, session_id, strlen(session_id));
+	buf += strlen(session_id) ;
+	total_length += 2 + strlen(session_id);
+
+	/*Frame-ip-address*/ 
+	*buf++ = 8;
+	*buf++ = 2 + sizeof(unsigned int);
+	framed_ip_addr = inet_addr("192.168.0.99");
+	/*printf("the ip addr is %s %d %d\n", inet_ntoa(framed_ip_addr), sizeof(framed_ip_addr), sizeof(unsigned int));*/
+	memcpy(buf, &framed_ip_addr, sizeof(unsigned int));
+	buf += sizeof(unsigned int);
+	total_length += 2 + sizeof(unsigned int);
+
+    /*acct-status-type*/ 
+	*buf++ = 40;
+	*buf++ = 2 + sizeof(unsigned int); 
+	lvalue = htonl(acct_status);
+	/**buf = 1;*/
+	memcpy(buf, (char *) &lvalue, sizeof(unsigned int));
+	total_length += 2 + sizeof(unsigned int);
+
+	total_length += 20;
+	auth->length = htons ((unsigned short) total_length);
+	memset((char *) auth->vector, 0, AUTH_VECTOR_LEN);
+	secretlen = strlen (secret);
+	memcpy ((char *) auth + total_length, secret, secretlen);
+	rc_md5_calc (vector, (char *) auth, total_length + secretlen);
+	memcpy(auth->vector, vector, AUTH_VECTOR_LEN);
+
+	sin = (struct sockaddr_in *) & saremote;
+	memset ((char *) sin, '\0', sizeof (saremote));
+	sin->sin_family = AF_INET;
+	/*sin->sin_addr.s_addr = inet_addr("192.168.0.99");*/
+	sin->sin_addr.s_addr = inet_addr("115.29.203.202");
+	sin->sin_port = htons(1813);
+
+	for (;;)
+	{
+		sendto (sockfd, (char *) auth, (unsigned int) total_length, (int) 0,
+			(struct sockaddr *) sin, sizeof (struct sockaddr_in));
+
+		authtime.tv_usec = 0L;
+		authtime.tv_sec = (long) 1;
+		FD_ZERO (&readfds);
+		FD_SET (sockfd, &readfds);
+		if (select (sockfd + 1, &readfds, NULL, NULL, &authtime) < 0) {
+			/*if (errno == EINTR)*/
+				/*continue;*/
+			/*error("rc_send_server: select: %m");*/
+			memset (secret, '\0', sizeof (secret));
+			close (sockfd);
+			return -1;
+		}
+		if (FD_ISSET (sockfd, &readfds))
+			break;
+	}
+	salen = sizeof (saremote);
+	length = recvfrom (sockfd, (char *) recv_buffer,
+			   (int) sizeof (recv_buffer),
+			   (int) 0, &saremote, &salen);
+
+	if (length <= 0)
+	{
+		close (sockfd);
+		memset (secret, '\0', sizeof (secret));
+		return -1;
+	}
+	recv_auth = (AUTH_HDR *)recv_buffer;
+	if (recv_auth->code == 5 && recv_auth->id == 1 ) {
+		printf("acct successfully\n");
+	}
+	else{
+		printf("acct failed\n");
+
+	}
+	/*result = rc_check_reply (recv_auth, 8192, secret, vector, 1);*/
+
+}
+
+int main(int argc, char **argv)
+{
+	char *username = "user1";
+	char *passwd   = "123456";
+
+	radius_pap_auth(username, passwd);
+	radius_acct_start(username);
 }
 
 
