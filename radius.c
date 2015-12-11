6dbc447b5b89a6be080135378c0e71d4bd48b0ef
hover <hover@netmoon.cn>
2015-12-11 09:11:54 +0800

    Add acctsessionid for every user

--- radius.c
+++ radius.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
 
 #define BUFFER_LEN              8192
 #define BADRESP_RC	-2
@@ -22,6 +25,9 @@
 #define MAX(a,b) (a >= b ? a : b)
 
 #define CHAP_VALUE_LENGTH		16
+/*#define RADIUS_ADDR	"121.41.81.117"*/
+/*#define RADIUS_ADDR	"121.41.81.117"*/
+/*#define RADIUS_ADDR	"121.41.81.117"*/
 
 typedef struct pw_auth_hdr {
 	u_char          code;
@@ -30,6 +36,20 @@ typedef struct pw_auth_hdr {
 	u_char          vector[AUTH_VECTOR_LEN];
 	u_char          data[2];
 } AUTH_HDR;
+
+typedef struct radius_user {
+	char *username;
+	unsigned int id;
+	int sockfd;
+	int mysql_times;
+	char *acctsessionid;
+} RADIUS_USER;
+
+static struct timeval tstart, tspend;
+static unsigned long mysql_num = 0;
+static unsigned long mysql_num_send = 0;
+static pthread_mutex_t mysql_num_lock;
+static float timeuse;
 
 static void rc_random_vector (unsigned char *vector)
 {
@@ -70,7 +90,7 @@ static void rc_random_vector (unsigned char *vector)
 	return;
 }
 
-static int radius_pap_auth(char *username, char *password, unsigned int id)
+static int radius_pap_auth(RADIUS_USER *radius_user)
 {
 	AUTH_HDR *auth, *recv_auth;
 	struct sockaddr salocal; struct sockaddr saremote;
@@ -82,7 +102,6 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 	unsigned char *buf;
 	unsigned char vector[AUTH_VECTOR_LEN];
 	unsigned char md5buf[256];
-	int sockfd;
 	unsigned int length;
 	unsigned char secret[49];
 	unsigned int total_length = 0;
@@ -93,6 +112,11 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 	int padded_length;
 	int secretlen;
 	int pc, i, j;
+
+	char *password   = "123456";
+	int sockfd = radius_user->sockfd;
+	char *username = radius_user->username;
+	unsigned int id = radius_user->id;
 
 	auth = malloc(sizeof(AUTH_HDR));
 	if (auth == (AUTH_HDR *)NULL){
@@ -106,11 +130,6 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 	/*auth->id = id % 256;*/
 	auth->id = rand()% 256;
 	
-	sockfd = socket (AF_INET, SOCK_DGRAM, 0);
-	if (sockfd < 0) {
-		printf("sockfd failed %s\n", username);
-		return; 
-	}
 
 	length = sizeof (salocal);
 	sin = (struct sockaddr_in *) & salocal;
@@ -118,16 +137,16 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 	sin->sin_family = AF_INET;
 	sin->sin_addr.s_addr = htonl(INADDR_ANY);
 	sin->sin_port = htons ((unsigned short) 0);
-	if (bind (sockfd, (struct sockaddr *) sin, length) < 0 ||
-		   getsockname (sockfd, (struct sockaddr *) sin, &length) < 0)
-	{
-		close (sockfd);
-		memset (secret, '\0', sizeof (secret));
-		/*error("rc_send_server: bind: %s: %m", server_name);*/
-		/*return (ERROR_RC);*/
-		printf("bind error\n");
-		return -1;
-	}
+
+	/*if (bind (sockfd, (struct sockaddr *) sin, length) < 0 ||*/
+		   /*getsockname (sockfd, (struct sockaddr *) sin, &length) < 0)*/
+	/*{*/
+		/*memset (secret, '\0', sizeof (secret));*/
+		/*[>error("rc_send_server: bind: %s: %m", server_name);<]*/
+		/*[>return (ERROR_RC);<]*/
+		/*printf("bind error\n");*/
+		/*return -1;*/
+	/*}*/
 
 	rc_random_vector(vector);
 	memcpy(auth->vector, vector, AUTH_VECTOR_LEN);
@@ -182,8 +201,9 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 	sin = (struct sockaddr_in *) & saremote;
 	memset ((char *) sin, '\0', sizeof (saremote));
 	sin->sin_family = AF_INET;
-	sin->sin_addr.s_addr = inet_addr("192.168.0.73");
+	/*sin->sin_addr.s_addr = inet_addr("192.168.1.33");*/
 	/*sin->sin_addr.s_addr = inet_addr("115.29.203.202");*/
+	sin->sin_addr.s_addr = inet_addr("121.41.81.117");
 	sin->sin_port = htons(1812);
 
 	for (j = 0; j < 3; j++)
@@ -193,7 +213,7 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 			(struct sockaddr *) sin, sizeof (struct sockaddr_in));
 
 		authtime.tv_usec = 0L;
-		authtime.tv_sec = (long) 2;
+		authtime.tv_sec = (long) 5;
 		FD_ZERO (&readfds);
 		FD_SET (sockfd, &readfds);
 		if (select (sockfd + 1, &readfds, NULL, NULL, &authtime) < 0) {
@@ -219,13 +239,13 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 			recv_auth = (AUTH_HDR *)recv_buffer;
 			/*printf("recv_auth->code = %d, recv_auth->id = %d\n", recv_auth->code, recv_auth->id);*/
 			if (recv_auth->code == 2 && recv_auth->id == auth->id ) {
-				close (sockfd);
-				/*printf("auth success %s\n", username);*/
+				/*close (sockfd);*/
+				/*printf("auth success: %s\n", username);*/
 				return 0;
 			}
 			else{
 				close (sockfd);
-				printf("auth failed %s\n", username);
+				printf("auth failed: %s\n", username);
 				return -1;
 
 			}
@@ -238,7 +258,7 @@ static int radius_pap_auth(char *username, char *password, unsigned int id)
 	return -1;
 }
 
-static int radius_acct_start(char *username, bool acct, unsigned int id)
+static int radius_acct_start(RADIUS_USER *radius_user, bool acct)
 {
 	AUTH_HDR *auth, *recv_auth;
 	struct sockaddr salocal; struct sockaddr saremote;
@@ -250,7 +270,6 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 	unsigned char *buf;
 	unsigned char vector[AUTH_VECTOR_LEN];
 	unsigned char md5buf[256];
-	int sockfd;
 	unsigned int length;
 	unsigned char secret[49];
 	unsigned int total_length = 0;
@@ -262,7 +281,6 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 	int secretlen;
 	int pc, i, j;
 
-	char *session_id = username;
 	int acct_status = 1;
 	int inter_update = 3;
 	unsigned int lvalue;
@@ -270,6 +288,11 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 	unsigned int framed_ip_addr;
 	unsigned int acct_input_octets;
 	unsigned int acct_output_octets;
+
+	int sockfd = radius_user->sockfd;
+	char *username = radius_user->username;
+	unsigned int id = radius_user->id;
+	char *session_id = radius_user->acctsessionid;
 
 	auth = malloc(sizeof(AUTH_HDR));
 	if (auth == (AUTH_HDR *)NULL){
@@ -283,28 +306,22 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 	/*auth->id = id % 256;*/
 	auth->id = rand() % 256;
 
-	sockfd = socket (AF_INET, SOCK_DGRAM, 0);
-	if (sockfd < 0) {
-		printf("sockfd failed %s\n", username);
-		return; 
-	}
-
 	length = sizeof (salocal);
 	sin = (struct sockaddr_in *) & salocal;
 	memset ((char *) sin, '\0', (size_t) length);
 	sin->sin_family = AF_INET;
 	sin->sin_addr.s_addr = htonl(INADDR_ANY);
 	sin->sin_port = htons ((unsigned short) 0);
-	if (bind (sockfd, (struct sockaddr *) sin, length) < 0 ||
-		   getsockname (sockfd, (struct sockaddr *) sin, &length) < 0)
-	{
-		close (sockfd);
-		memset (secret, '\0', sizeof (secret));
-		/*error("rc_send_server: bind: %s: %m", server_name);*/
-		/*return (ERROR_RC);*/
-		printf("bind error\n");
-		return -1;
-	}
+	/*if (bind (sockfd, (struct sockaddr *) sin, length) < 0 ||*/
+		   /*getsockname (sockfd, (struct sockaddr *) sin, &length) < 0)*/
+	/*{*/
+		/*close (sockfd);*/
+		/*memset (secret, '\0', sizeof (secret));*/
+		/*[>error("rc_send_server: bind: %s: %m", server_name);<]*/
+		/*[>return (ERROR_RC);<]*/
+		/*printf("bind error\n");*/
+		/*return -1;*/
+	/*}*/
 
 	strcpy(secret, MGMT_POLL_SECRET);
 
@@ -347,7 +364,8 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 	/* Acct-Input-Octets*/ 
 	*buf++ = 42;
 	*buf++ = 2 + sizeof(unsigned int); 
-	acct_input_octets = htonl(1000);
+	/*acct_input_octets = htonl(1000);*/
+	acct_input_octets = htonl(time(NULL));
 	memcpy(buf, (char *) &acct_input_octets, sizeof(unsigned int));
 	buf += sizeof(unsigned int);
 	total_length += 2 + sizeof(unsigned int);
@@ -371,8 +389,9 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 	sin = (struct sockaddr_in *) & saremote;
 	memset ((char *) sin, '\0', sizeof (saremote));
 	sin->sin_family = AF_INET;
-	sin->sin_addr.s_addr = inet_addr("192.168.0.73");
+	/*sin->sin_addr.s_addr = inet_addr("192.168.1.33");*/
 	/*sin->sin_addr.s_addr = inet_addr("115.29.203.202");*/
+	sin->sin_addr.s_addr = inet_addr("121.41.81.117");
 	sin->sin_port = htons(1813);
 
 	for (j = 0; j < 3; j++)
@@ -382,7 +401,7 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 			(struct sockaddr *) sin, sizeof (struct sockaddr_in));
 
 		authtime.tv_usec = 0L;
-		authtime.tv_sec = (long) 2;
+		authtime.tv_sec = (long) 5;
 		FD_ZERO (&readfds);
 		FD_SET (sockfd, &readfds);
 		if (select (sockfd + 1, &readfds, NULL, NULL, &authtime) < 0) {
@@ -408,11 +427,11 @@ static int radius_acct_start(char *username, bool acct, unsigned int id)
 			}
 			recv_auth = (AUTH_HDR *)recv_buffer;
 			if (recv_auth->code == 5 && recv_auth->id == auth->id) {
-				/*printf("acct successfully\n");*/
-				close (sockfd);
+				/*printf("acct successfully: %s\n", username);*/
+				/*close (sockfd);*/
 				return 0;
 			} else{
-				printf("acct failed\n");
+				printf("acct failed: %s\n", username);
 				close (sockfd);
 				return -1;
 			}
@@ -430,28 +449,81 @@ static void radius_acct(unsigned int *id)
 {
 	char buf[10];
 	char *name = "user";
-	char *passwd   = "123456";
 	unsigned int user_id = *id;
+	int sockfd;
 	char *username;
+	int ret;
+	RADIUS_USER *radius_user;
+
+	char acctsessionid[128];
+
+
+	radius_user = malloc(sizeof(RADIUS_USER));
+	if (radius_user == (RADIUS_USER*)NULL){
+		printf("alloc radius user failed\n");
+		return -1;
+	}
 
 	sprintf(buf, "%s%d", name, *id);
 	username = buf;
-	while(1) {
-		if (radius_pap_auth(username, passwd, user_id) == 0) {
-				radius_acct_start(username, 0, user_id);
-				while(1) {
-					sleep(60);
-					radius_acct_start(username, 1, user_id);
-				}
-			}
+	sprintf(acctsessionid, "%s-%d", username, time(NULL));
+	radius_user->acctsessionid = acctsessionid;
+
+	sockfd = socket (AF_INET, SOCK_DGRAM, 0);
+	radius_user->sockfd = sockfd;
+	radius_user->username = username;
+	radius_user->id = id;
+	radius_user->mysql_times = 0;
+	if (sockfd < 0) {
+		printf("sockfd failed %s\n", username);
+		return; 
 	}
+
+	if (radius_pap_auth(radius_user) == 0) {
+			radius_acct_start(radius_user, 0);
+			pthread_mutex_lock(&mysql_num_lock);
+			mysql_num_send += 2;
+			mysql_num += 2;
+			pthread_mutex_unlock(&mysql_num_lock);
+
+			while(1) {
+				usleep(1000 * 10);
+				ret = radius_acct_start(radius_user, 1);
+				radius_user->mysql_times++;
+				pthread_mutex_lock(&mysql_num_lock);
+				mysql_num_send++;
+				if (ret == 0)
+					mysql_num++;
+				pthread_mutex_unlock(&mysql_num_lock);
+			}
+		}
+}
+
+static void stop_func(void)
+{
+	gettimeofday(&tspend, NULL);
+	
+	timeuse = 1000000*(tspend.tv_sec - tstart.tv_sec) + (tspend.tv_usec - tstart.tv_usec);
+	timeuse /= 1000000;
+	printf("request Sended: %d, request succeed: %d, timeuse: %f, request/second: %f, time = %d\n",mysql_num_send, mysql_num, timeuse, mysql_num/timeuse, time(NULL));
+	exit(0);
+}
+
+void sigalrm_func(void)
+{
+	raise(SIGINT);
+}
+
+void thread_time(void)
+{
+	gettimeofday(&tstart, NULL);
 }
 
 int main(int argc, char **argv)
 {
 	int i;
 	
-	if (argc !=2){
+	if (argc !=3){
 		printf("Usage: ./radius user_num\n");
 		return;
 	}
@@ -459,6 +531,13 @@ int main(int argc, char **argv)
 	unsigned int user_num = atoi(argv[1]);
 	pthread_t thread_id[user_num];
 	unsigned int j[user_num];
+	pthread_mutex_init(&mysql_num_lock, NULL);
+
+	thread_time();
+
+	signal(SIGALRM, sigalrm_func);
+	alarm(atoi(argv[2]));
+	signal(SIGINT, stop_func);
 
 	for(i = 0; i < user_num; i++) {
 		j[i] = i;
@@ -467,10 +546,11 @@ int main(int argc, char **argv)
 			continue;
 		}
 	}
+
 	while(1)
 	{
 		sleep(5);
-		printf("-----------\n");
+		/*printf("-----------\n");*/
 	}
 
 }
